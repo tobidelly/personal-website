a9b6e34ea2975864bf4127d3b01f92107454372b
tobidelly <tobidelly@gmail.com>
2024-09-12 09:05:10 +0100

    Moved the About me page to Public and change the file to beyond-coding to redirect Vite to Static HTML

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    fs: {
+      allow: ['src', 'public', 'node_modules'] // Include 'src', 'public', and 'node_modules' directories
+    }
+  },
   plugins: [react()],
-  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg'],
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.svg']
 });
