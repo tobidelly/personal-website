1f2aab20f4fce7139b4df106b9050d2566ef680a
tobidelly <tobidelly@gmail.com>
2024-09-12 06:26:43 +0100

    Updated Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Eskinder &nbsp;{' '}
-            <span className="sm:block hidden">| &nbsp;AI Engineer</span>
+            TD! &nbsp;{' '}
+            <span className="sm:block hidden">| &nbsp;Software Engineer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
